06445ff451c1435736c062a7bac72e9b6940270e
JStoppe <jonathan.stoppe@gmail.com>
2022-05-10 09:51:43 +0200

    blockMinedReward checkChainvalid()

--- src/main/java/de/muc/vaulty/FullNode.java
+++ src/main/java/de/muc/vaulty/FullNode.java
@@ -50,13 +50,77 @@ public class FullNode extends Node implements Serializable{
 		// delete Transactions included in this Block
 	}
 	
-	private boolean isChainValid() {
-		for(int i = 0; i<blockchain.size(); i++) {
-			if(!isBlockValid(blockchain.get(i)))
+	public Boolean isChainValid() {
+		Block currentBlock; 
+		Block previousBlock;
+		String hashTarget = new String(new char[VaultyChain.difficulty]).replace('\0', '0');
+		HashMap<String,TransactionOutput> tempUTXOs = new HashMap<String,TransactionOutput>(); //a temporary working list of unspent transactions at a given block state.
+		
+		
+		//loop through blockchain to check hashes:
+		for(int i=1; i < blockchain.size(); i++) {
+			if(i==1) {
+				tempUTXOs.put(blockchain.get(0).transactions.get(0).transactionId, blockchain.get(0).transactions.get(0).outputs.get(0));
+			}
+			currentBlock = blockchain.get(i);
+			previousBlock = blockchain.get(i-1);
+			//compare registered hash and calculated hash:
+			if(!currentBlock.hash.equals(StringUtil.calculateHash(currentBlock)) ){
+				System.out.println("#Current Hashes not equal");
 				return false;
-			if(i!=0)
-				if(blockchain.get(i).previousHash != blockchain.get(i-1).previousHash)
+			}
+			//compare previous hash and registered previous hash
+			if(!previousBlock.hash.equals(currentBlock.previousHash) ) {
+				System.out.println("#Previous Hashes not equal");
+				return false;
+			}
+			//check if hash is solved
+			if(!currentBlock.hash.substring( 0, VaultyChain.difficulty).equals(hashTarget)) {
+				System.out.println("#This block hasn't been mined");
+				return false;
+			}
+			
+			//loop through blockchains transactions:
+			TransactionOutput tempOutput;
+			for(int t=0; t <currentBlock.transactions.size(); t++) {
+				Transaction currentTransaction = currentBlock.transactions.get(t);
+				
+				if(!StringUtil.verifySignature(currentTransaction)) {
+					System.out.println("#Signature on Transaction(" + t + ") is Invalid");
+					return false; 
+				}
+				
+				for(TransactionOutput input : currentTransaction.inputs) {	
+					tempOutput = tempUTXOs.get(input);
+					
+					if(tempOutput == null) {
+						System.out.println("#Referenced input on Transaction(" + t + ") is Missing");
+						return false;
+					}
+					
+					if(input.value != tempOutput.value) {
+						System.out.println("#Referenced input Transaction(" + t + ") value is Invalid");
+						return false;
+					}
+					
+					tempUTXOs.remove(input);
+				}
+				
+				for(TransactionOutput output: currentTransaction.outputs) {
+					tempUTXOs.put(output.id, output);
+				}
+				
+				if( currentTransaction.outputs.get(0).reciepient != currentTransaction.reciepient) {
+					System.out.println("#Transaction(" + t + ") output reciepient is not who it should be");
 					return false;
+				}
+				if( currentTransaction.outputs.get(1).reciepient != currentTransaction.sender) {
+					System.out.println("#Transaction(" + t + ") output 'change' is not sender.");
+					return false;
+				}
+				
+			}
+			
 		}
 		return true;
 	}
